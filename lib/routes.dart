84eadc5f4100ab94c8039b28e8d914b6af3dbc4a
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2021-04-16 07:37:46 +0700

    https://www.youtube.com/

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,15 @@
-import 'file:///E:/Android%20Studio/EZ_shop/E-commerce-Mobile-Customer/lib/screens/home/home.dart';
 import 'package:customer_app/screens/loading.dart';
+import 'package:customer_app/screens/home/home.dart';
 import 'package:customer_app/screens/login.dart';
+import 'package:customer_app/screens/search.dart';
 import 'package:customer_app/screens/welcome/welcome_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   Home.routeName: (context) => Home(),
+  Search.routeName: (context) => Search(),
   Loading.routeName: (context) => Loading(),
   Login.routeName: (context) => Login(),
   WelcomeScreen.routeName: (context) => WelcomeScreen(),
-  Loading.routeName: (context) => Loading()
+  Loading.routeName: (context) => Loading(),
 };
